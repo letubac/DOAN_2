8a25ebb10ba5904c97e472e9bd04e6ae45cd7bea
azuretran <56498719+azuretran@users.noreply.github.com>
2021-06-15 13:10:38 +0700

    Update urls.py

--- student_management_app/urls.py
+++ student_management_app/urls.py
@@ -33,8 +33,14 @@ urlpatterns = [
     path('add_student_save/', HodViews.add_student_save, name="add_student_save"),
     path('edit_student/<student_id>', HodViews.edit_student, name="edit_student"),
     path('edit_student_save/', HodViews.edit_student_save, name="edit_student_save"),
+     path('add_saff_gv/', HodViews.add_saff_gv, name="add_saff_gv"),
+    path('add_saff_gv_save/', HodViews.add_saff_gv_save, name="add_saff_gv_save"),
+    path('edit_staff_gv/<staff_gv_id>', HodViews.edit_staff_gv, name="edit_staff_gv"),
+    path('edit_staff_gv_save/', HodViews.edit_staff_gv_save, name="edit_staff_gv_save"),
     path('manage_student/', HodViews.manage_student, name="manage_student"),
     path('delete_student/<student_id>/', HodViews.delete_student, name="delete_student"),
+     path('manage_staff_gv/', HodViews.manage_staff_gv, name="manage_staff_gv"),
+    path('delete_staff_gv/<staff_gv_id>/', HodViews.delete_staff_gv, name="delete_staff_gv"),
     path('add_subject/', HodViews.add_subject, name="add_subject"),
     path('add_subject_save/', HodViews.add_subject_save, name="add_subject_save"),
     path('manage_subject/', HodViews.manage_subject, name="manage_subject"),
@@ -47,15 +53,6 @@ urlpatterns = [
     path('student_feedback_message_reply/', HodViews.student_feedback_message_reply, name="student_feedback_message_reply"),
     path('staff_feedback_message/', HodViews.staff_feedback_message, name="staff_feedback_message"),
     path('staff_feedback_message_reply/', HodViews.staff_feedback_message_reply, name="staff_feedback_message_reply"),
-    path('student_leave_view/', HodViews.student_leave_view, name="student_leave_view"),
-    path('student_leave_approve/<leave_id>/', HodViews.student_leave_approve, name="student_leave_approve"),
-    path('student_leave_reject/<leave_id>/', HodViews.student_leave_reject, name="student_leave_reject"),
-    path('staff_leave_view/', HodViews.staff_leave_view, name="staff_leave_view"),
-    path('staff_leave_approve/<leave_id>/', HodViews.staff_leave_approve, name="staff_leave_approve"),
-    path('staff_leave_reject/<leave_id>/', HodViews.staff_leave_reject, name="staff_leave_reject"),
-    path('admin_view_attendance/', HodViews.admin_view_attendance, name="admin_view_attendance"),
-    path('admin_get_attendance_dates/', HodViews.admin_get_attendance_dates, name="admin_get_attendance_dates"),
-    path('admin_get_attendance_student/', HodViews.admin_get_attendance_student, name="admin_get_attendance_student"),
     path('admin_profile/', HodViews.admin_profile, name="admin_profile"),
     path('admin_profile_update/', HodViews.admin_profile_update, name="admin_profile_update"),
     
@@ -79,15 +76,15 @@ urlpatterns = [
     path('staff_add_result/', StaffViews.staff_add_result, name="staff_add_result"),
     path('staff_add_result_save/', StaffViews.staff_add_result_save, name="staff_add_result_save"),
 
-    # URSL for Student
+    # URL for Student
     path('student_home/', StudentViews.student_home, name="student_home"),
-    path('student_view_attendance/', StudentViews.student_view_attendance, name="student_view_attendance"),
-    path('student_view_attendance_post/', StudentViews.student_view_attendance_post, name="student_view_attendance_post"),
-    path('student_apply_leave/', StudentViews.student_apply_leave, name="student_apply_leave"),
-    path('student_apply_leave_save/', StudentViews.student_apply_leave_save, name="student_apply_leave_save"),
     path('student_feedback/', StudentViews.student_feedback, name="student_feedback"),
     path('student_feedback_save/', StudentViews.student_feedback_save, name="student_feedback_save"),
     path('student_profile/', StudentViews.student_profile, name="student_profile"),
     path('student_profile_update/', StudentViews.student_profile_update, name="student_profile_update"),
-    path('student_view_result/', StudentViews.student_view_result, name="student_view_result"),
+    path('student_register_course/', StudentViews.student_register_course, name="student_register_course"),
+    path('student_view_timetable/', StudentViews.student_view_timetable, name="student_view_timetable"),
+    path('student_view_diploma/', StudentViews.student_view_diploma, name="student_view_diploma"),
+
+
 ]
